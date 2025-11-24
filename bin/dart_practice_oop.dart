dbc80a2bf7e5f485d8111eea21681830c3e7de28
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-11-24 16:47:17 +0500

    n-Fibonachi sonini rekursiv funksiya yordamida aniqlovchi dastur tuzing. Bunda: f(0) = 0, f(1) = 1, f(n) = f(n-1)+f(n-2), n>=2.

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,9 +1,16 @@
 void main() {
-  print(calculateSumRecursive(5));
+  print(fibonacciRecursive(10));
 }
 
-int calculateSumRecursive(int n){
-if(n == 0) return 0;
+int fibonacciRecursive(int n){
 
-return n + calculateSumRecursive( n - 1);
+if(n < 0) throw ArgumentError("Fibonachchi indeksi (n) manfiy bo'lmasligi kerak.");
+
+if(n == 0) return 0;
+if(n == 1) return 1;
+
+return fibonacciRecursive( n - 1) + fibonacciRecursive(n - 2);
+
+
+
 }
