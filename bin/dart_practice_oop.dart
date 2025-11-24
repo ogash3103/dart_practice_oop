b6ff0fd3c3b1d95922e4ff8bf208404b8d539c9a
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-11-24 18:03:54 +0500

    Product nomli class hosil qiling, u quyidagi fieldlardan iborat bo’lsin. Class constructorini named parameter orqali hosil qiling! • id • brand • name • price • quantity • description • soldQuantity • sizes • colors • category

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,30 +1,61 @@
-
-
 void main(){
-  List<int> list1 = [1, 2, 3, 1, 4, 2, 5];
-  List<int> result1 = changeToZero(list1);
+  Product laptop = Product(
+    id: 101,
+    name: "MacBook Pro M3",
+    brand: "Apple",
+    category: "Electronics",
+    price: 1999.99,
+    quantity: 50,
+    soldQuantity: 15,
+    sizes: ["14 inch", "16 inch"],
+    colors: ["Space Gray", "Silver"],
+    description: "Yangi avlod M4 pro chipiga ega kuchli noutbuk.",
+  );
 
-  print('changeToZero($list1) -> $result1');
+  print(laptop);
 }
 
-List<int> changeToZero(List<int> numbers){
+class Product{
+  int id;
+  String brand;
+  String name;
+  double price;
+  int quantity;
+  String? description;
+  int soldQuantity;
+  List<String> sizes;
+  List<String> colors;
+  String category;
 
-  Map<int, int> frequency = {};
+  Product({
+    required this.id,
+    required this.brand,
+    required this.name,
+    required this.price,
+    required this.quantity,
+    required this.soldQuantity,
+    required this.sizes,
+    required this.colors,
+    required this.category,
 
-  for(var number in numbers)
-    {
-      frequency[number] = (frequency[number] ?? 0) + 1;
-    }
-
-  List<int> result = numbers.map((number){
-
-    if(frequency[number]! > 1){
-      return 0;
-    }else{
-      return number;
-    }
-  }).toList();
-
-  return result;
+    this.description,
+  }) :
+        assert(id > 0),
+        assert(price >= 0.0),
+        assert(quantity >= 0);
+  @override
+  String toString() {
+    return 'Product(\n'
+        '  id: $id, \n'
+        '  brand: $brand, \n'
+        '  name: $name, \n'
+        '  price: $price, \n'
+        '  quantity: $quantity, \n'
+        '  soldQuantity: $soldQuantity, \n'
+        '  sizes: $sizes, \n'
+        '  colors: $colors, \n'
+        '  category: $category, \n'
+        '  description: ${description ?? 'N/A'}\n'
+        ')';
+  }
 }
-
