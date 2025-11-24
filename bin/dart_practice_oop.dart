421f59c603e2f14581d96631d741cca29f7e7b98
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-11-24 16:39:31 +0500

    1 dan N gacha bo’lgan natural sonlarning yig’indisini rekursiv funksiya yordamida hisoblang.

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,26 +1,9 @@
-void main()
-{
- print(ekub(12, 18, 34, 48, 56));
+void main() {
+  print(calculateSumRecursive(5));
 }
 
-int gcd2(int a, int b){
-a = a.abs();
-b = b.abs();
+int calculateSumRecursive(int n){
+if(n == 0) return 0;
 
-while(b != 0)
-  {
-    int temp = b;
-    b = a % b;
-    a = temp;
-  }
-  return a;
-}
-
-int ekub(int a, int b, [int? c, int? d, int? e]){
-  int result = gcd2(a, b);
-  if(c != null) result = gcd2(result, c);
-  if(d != null) result = gcd2(result, d);
-  if(e != null) result = gcd2(result, e);
-
-  return result;
+return n + calculateSumRecursive( n - 1);
 }
