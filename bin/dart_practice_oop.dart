2aefef82629b1608b9e70afeed4b58a29050e656
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-11-25 04:26:19 +0500

    N natural soni va satr berilgan. Uzunligi N ga teng bo'lgan satri shunday hosil qilingki, agar kiritilgan satrning uzunligi N dan katta bo'lsa, satrni o’zgarishsiz qoldiring. Agar kiritilgan satrning uzunligi N dan kichik bo'lsa, yangi satr boshiga nuqtalarni “.”

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,61 +1,11 @@
-void main(){
-  Product laptop = Product(
-    id: 101,
-    name: "MacBook Pro M3",
-    brand: "Apple",
-    category: "Electronics",
-    price: 1999.99,
-    quantity: 50,
-    soldQuantity: 15,
-    sizes: ["14 inch", "16 inch"],
-    colors: ["Space Gray", "Silver"],
-    description: "Yangi avlod M4 pro chipiga ega kuchli noutbuk.",
-  );
-
-  print(laptop);
-}
-
-class Product{
-  int id;
-  String brand;
-  String name;
-  double price;
-  int quantity;
-  String? description;
-  int soldQuantity;
-  List<String> sizes;
-  List<String> colors;
-  String category;
-
-  Product({
-    required this.id,
-    required this.brand,
-    required this.name,
-    required this.price,
-    required this.quantity,
-    required this.soldQuantity,
-    required this.sizes,
-    required this.colors,
-    required this.category,
-
-    this.description,
-  }) :
-        assert(id > 0),
-        assert(price >= 0.0),
-        assert(quantity >= 0);
-  @override
-  String toString() {
-    return 'Product(\n'
-        '  id: $id, \n'
-        '  brand: $brand, \n'
-        '  name: $name, \n'
-        '  price: $price, \n'
-        '  quantity: $quantity, \n'
-        '  soldQuantity: $soldQuantity, \n'
-        '  sizes: $sizes, \n'
-        '  colors: $colors, \n'
-        '  category: $category, \n'
-        '  description: ${description ?? 'N/A'}\n'
-        ')';
+void main() {
+  int n = 5;
+  String text = 'Dart';
+  if(text.length >= n) {
+    print(text);
+  }else if(text.length < n) {
+    print(text.padLeft(n, '.'));
+  }else {
+    print('xato');
   }
 }
