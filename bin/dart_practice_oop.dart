85b14ad9e4392ca954142b4c76bb559df55c74de
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-11-25 20:41:50 +0500

    Twitterda chiqarilgan postda yozilgan username(@ - belgi bilan boshlanuvchi identifikator) va hashtag(# - belgi bilan boshlanuvchi identifikator) larni uppercasega o’tkazuvchi funksiya tuzing. Input: Just two weeks until the @TheBookerPrizes shortlist is announced! http://bit.ly/bookerprizebso Explore the longlist and support indie bookshops #2021BookerPrize #FinestFiction #ChooseIndieLinks Output: Just two weeks until the @THEBOOKERPRIZES shortlist is announced! http://bit.ly/bookerprizebso Explore the longlist and support indie bookshops #2021BOOKERPRIZE #FINESTFICTION #CHOOSEINDIELINKS

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,11 +1,17 @@
 void main(){
-  print(fistThree('sa'));
+  String input = """Just two weeks until the @TheBookerPrizes shortlist is 
+announced! http://bit.ly/bookerprizebso Explore the longlist and support 
+indie bookshops #2021BookerPrize #FinestFiction #ChooseIndieLinks""";
+
+  String output = convertTags(input);
+
+  print(output);
 }
 
-String fistThree(String str){
-  if(str.length >= 3){
-    return str.substring(0,3);
-  }else{
-    return str.padRight(3, '@');
-  }
+String convertTags(String str){
+  final reg = RegExp(r'[@#][A-Za-z0-9_]+');
+
+  return str.replaceAllMapped(reg, (match){
+          return match.group(0)!.toUpperCase();
+  });
 }
