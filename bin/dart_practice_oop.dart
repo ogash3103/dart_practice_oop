52f0056186458eac5df699468f652338ef26c659
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-11-25 07:20:23 +0500

    Satr berilgan. Satrdagi raqamlar sonini aniqlovchi funksiya tuzilsin. Funksiya named parametr orqali argumentni talab qilib olsin.

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,14 +1,16 @@
 void main(){
-  String str1 = "Hello PDP Academy";
-  String str2 = "PDP";
-
-  int index = str1.indexOf(str2);
-
-  if(index == -1){
-    print(str1);
-  }else{
-    String result = str1.replaceFirst(str2, "Friend");
-    print(result);
-  }
+  String text = "2345gtn35";
+  int result = countDigits(num: text);
+  print('Raqamlar soni: $result');
 }
-  
+
+int countDigits({required String num}){
+  int count = 0;
+  for(var ch in num.split('')){
+    if(ch.codeUnitAt(0) >= 48 && ch.codeUnitAt(0) <= 57){
+      count++;
+    }
+  }
+
+  return count;
+}
