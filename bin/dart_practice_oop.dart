f6807198f0da97087474376ddb5085927776f620
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-11-27 21:36:28 +0500

    Butun n soni berilgan. Agar berilgan butun son 3 ga goldiqsiz bo 'linsa "Fizz" so’zi, 5 ga qoldiqsiz bo ‘linsa "Buzz" so’zi, agarda ham 3 ga va 5 ga goldiqsiz bo'linsa "FizzBuzz" so'zini son o'rniga chiqaruvchi funksiya tuzing. Agar berilgan son 3 ga ham 5 ga ham bo’linmasa sonni o’zini chiqarsin.Sonni 3 ga va 5 ga bo’linishini tekshiruvchi checkDevide(int n, int div) nomli funksiya tuzing va uni umumiy fizzBuzz funkisyaga parametr sifatida qabul qilib oling. Example: n = 17 => fizzBuzz(number: n, checker: checkDevide) => 17

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,13 +1,30 @@
 void main(){
-  int Function(int) bunny = bunnyEars;
-  print(bunny(5));
+  List<int> num = [1, 3, 5, 15, 17, 30];
+
+  for(int n in num){
+    String result = fizzBuzz(number: n, checker: checkDevide);
+    print("n = $n => $result");
+  }
 
 }
 
-int bunnyEars(int bunnies){
-  if(bunnies == 0){
-    return 0;
+bool checkDevide(int n, int dive) => n % dive == 0;
+
+String fizzBuzz({
+required int number,
+required bool Function(int n, int dive) checker}){
+
+  final isDivisibleBy3 = checker(number, 3);
+  final isDivisibleBy5 = checker(number, 5);
+
+  if(isDivisibleBy3 && isDivisibleBy5){
+    return "FizzBuzz";
+  }else if(isDivisibleBy3){
+    return 'Fizz';
+  } else if(isDivisibleBy5){
+    return 'Buzz';
+  }else {
+    return number.toString();
   }
 
-  return 2 + bunnyEars(bunnies - 1);
 }
