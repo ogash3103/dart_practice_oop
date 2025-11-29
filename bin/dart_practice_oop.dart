534cd1620097dd9da9df94bf969f032b183c9c86
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-11-29 09:16:03 +0500

    n sonining raqamlar yig'indisini takrorlanish operatoridan foydalanmagan holda hisoblovchi int turidagi digitSum(n) rekursiv funksiyasi tuzilsin. Shu funksiya yordamida berilgan butun sonning ragamlari yig’indisi topilsin.

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,23 +1,20 @@
 void main(){
-  String s2 = "Janob 420 filmi 19:00 da boshlanadi";
-  int natija2 = digitCount(s2);
-  print("S = \"$s2\"");
-  print("Natija: $natija2\n");
+  int Function(int) sum;
+  sum = digitSum;
+  print(sum(12345));
 }
 
-bool isDigit(String char){
-  return char.isNotEmpty &&
-      char.codeUnitAt(0) >= '0'.codeUnitAt(0)
-      && char.codeUnitAt(0) <=  '9'.codeUnitAt(0);
-}
+int digitSum(int n){
+  if(n < 0){
+    n = -n;
+  }
 
-int digitCount(String s){
-  if(s.isEmpty) return 0;
+  if(n == 0) return 0;
 
-  String str1 = s[0];
-  String qolganSonlar = s.substring(1);
+  int oxirgiNum = n % 10;
+  print('oxirgiNum: $oxirgiNum');
+  int qolganNum = n ~/ 10;
+  print('qolganNum: $qolganNum');
 
-  int hisob = isDigit(str1) ? 1 : 0;
-
-  return hisob + digitCount(qolganSonlar);
+  return oxirgiNum + digitSum(qolganNum);
 }
