6a5362bd3cf3b484fae6646717add3b4bd13fe7d
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-11-30 19:35:09 +0500

    Berilgan sartni katta lotin harflarini kichikga, kichiklarini katta harflarga aylantiruvchi funksiya tuzilsin. Example: “FLuTTeR” => “flUttEr”

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,6 +1,19 @@
 void main(){
-print(reverseString('salom'));
+print(swapCase('FLuTTeR'));
   
 }
 
-String reverseString(String s) => s.split('').reversed.join('');
+String swapCase(String s){
+  final buffer = StringBuffer();
+
+  for(var ch in s.split('')){
+    if(ch.contains(RegExp(r'[A-Za-z]'))){
+      buffer.write(
+        ch == ch.toUpperCase() ? ch.toLowerCase() : ch.toUpperCase()
+      );
+    }else {
+      buffer.write(ch);
+    }
+  }
+  return buffer.toString();
+}
