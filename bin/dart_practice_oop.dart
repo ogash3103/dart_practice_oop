1dbf8eb91bf7e3e2c223a4e4caef480118425eb6
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-11-30 19:24:10 +0500

    Kiritilgan satrni teskari tartibda o’giruvchi return typeli funksiya tuzilsin.

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,9 +1,6 @@
 void main(){
-  List<int> nums1 = [0, 2, 1, 5, 3, 4];
-  List<int> ans1 = buildArray(nums1);
-
-  print("Input: nums = $nums1");
-  print("Output: $ans1");
+print(reverseString('salom'));
+  
 }
 
-List<int> buildArray(List<int> nums) => List.generate(nums.length, (i) => nums[nums[i]]);
+String reverseString(String s) => s.split('').reversed.join('');
