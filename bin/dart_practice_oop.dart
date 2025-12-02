6b79ed610f144918f4cc80ed9a334b92df06720f
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-12-02 23:06:36 +0500

    N ta butun sondan tashlik topgan o’zgarmas list berilgan. Listdagi toq sonlar ko’paytmasidan juft sonlar yig’indisini ayirib natijani chiqaruvchi dastur tuzing.

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,23 +1,13 @@
-void printSpecialOrder(List<int> num){
- int left = 0;
- int right = num.length - 1;
- bool takeFromLeft = true;
-
- while(left <= right){
-   if(takeFromLeft){
-     if(left <= right) print(num[left++]);
-     if(left <= right) print(num[left++]);
-   } else {
-     if (right >= left) print(num[right--]);
-     if (right >= left) print(num[right--]);
-   }
-
-   takeFromLeft = !takeFromLeft;
- }
-}
-
 void main(){
-  List<int> nums = [10, 20, 30, 40, 50, 60, 70, 80];
-  printSpecialOrder(nums);
+  const List<int> list = [2, 5, 3, 8, 7, 4];
+
+  final oddProduct = list.where((x) => x.isOdd)
+      .fold<int>(1, (prod, x) => prod * x);
+
+  final evenSum = list.where((x) => x.isEven)
+      .fold<int>(0, (sum, x) => sum + x);
+
+  final result = oddProduct - evenSum;
+  print(result);
 
 }
