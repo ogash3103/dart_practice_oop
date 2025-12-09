6bf9a4bb916bfae57946ba3eaa19df27688aff2b
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-12-09 20:15:40 +0500

    Biror tizim azosini ifodalovchi Member class hosil qiling, class da “free”, “premiumGold” va “premiumSilver” toifali memberlarni hosil qiluvchi “redirecting” konstruktorlarni hosil qildim

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,32 +1,36 @@
-import 'dart:convert';
 
-void main(){
- final json = {
-   'userId':  1,
-   'id': 1,
-   'title': 'heelo'
- };
-
- final user = User.fromJson(json);
- print(user.title);
-
-
-
-
+enum MemberType {
+  free,
+  premiumGold,
+  premiumSilver,
 }
 
-class User
-{
-  int userId;
-  int id;
-  String title;
+class Member {
+  final int id;
+  final String name;
+  final MemberType type;
 
+  // ✅ ASOSIY constructor
+  Member(this.id, this.name, this.type);
 
-  User.fromJson(Map<String, Object?> json) :
-      userId = json['userId'] as int,
-      id = json['id'] as int,
-      title = json['title'] as String {
-    print('json to object');
-  }
+  // ✅ FREE member
+  Member.free(int id)
+      : this(id, 'Free Member', MemberType.free);
 
+  // ✅ PREMIUM GOLD member
+  Member.premiumGold(int id)
+      : this(id, 'Premium Gold Member', MemberType.premiumGold);
+
+  // ✅ PREMIUM SILVER member
+  Member.premiumSilver(int id)
+      : this(id, 'Premium Silver Member', MemberType.premiumSilver);
+}
+void main() {
+  final m1 = Member.free(1);
+  final m2 = Member.premiumGold(2);
+  final m3 = Member.premiumSilver(3);
+
+  print('${m1.id} | ${m1.name} | ${m1.type}');
+  print('${m2.id} | ${m2.name} | ${m2.type}');
+  print('${m3.id} | ${m3.name} | ${m3.type}');
 }
