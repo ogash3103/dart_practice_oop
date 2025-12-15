227a728778e8b9b1c7f5f23ea79938b671000091
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-12-15 20:22:45 +0500

    enum bilan practice with func

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,7 +1,24 @@
 import 'package:dart_practice_oop/audio_player.dart';
-
+enum Direction{
+  north,
+  south,
+  east,
+  west
+}
 void main(){
   final audio = AudioPlayer();
 
   audio.play();
+print(getDirectionInfo(Direction.north));
 }
+
+String getDirectionInfo(Direction direction){
+
+  switch(direction){
+    case Direction.north: return "Shimol (North)";
+    case Direction.south: return "Janub (South)";
+    case Direction.east:  return "Sharq (East)";
+    case Direction.west:  return "G‘arb (West)";
+  }
+}
+
