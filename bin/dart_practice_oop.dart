53478a1594f046a105994670abebe7526dc1a8c9
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-12-19 19:02:43 +0500

    practice

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,8 +1,25 @@
-import 'package:dart_practice_oop/Singleton/singleton_.dart';
+class Date {
+  int year;
+  int month;
+  int day;
+
+  Date(this.year, this.month, this.day);
+
+  bool operator >(Date other) =>
+      year > other.year || (year == other.year && month > other.month) ||
+          (year == other.year && month == other.month && day > other.day);
+
+  @override
+  String toString()  => "$year, $month, $day";
+}
 
 void main(){
-  print(AppConfig.init(
-      apiKey: "KEY_123",
-      serverUrl: "https://api.app.com",
-      isDebug: true));
+
+  final d1 = Date(2025, 12, 19);
+  final d2 = Date(2024, 12, 31);
+  final d3 = Date(2025, 11, 20);
+
+  print(d1 > d2); // true
+  print(d1 > d3); // true
+  print(d3 > d1); // false
 }
