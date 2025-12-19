c79bb43346f541521b7fd727957bb2458a754022
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-12-19 19:10:22 +0500

    Point klassini hosil qiling, uni x va y fieldlari bo’lsin. Shu klass uchun “+” va “-“ operatorlarini yozing.

--- bin/dart_practice_oop.dart
+++ bin/dart_practice_oop.dart
@@ -1,25 +1,22 @@
-class Date {
-  int year;
-  int month;
-  int day;
+class Print{
+ int x;
+ int y;
 
-  Date(this.year, this.month, this.day);
+ Print(this.x, this.y);
 
-  bool operator >(Date other) =>
-      year > other.year || (year == other.year && month > other.month) ||
-          (year == other.year && month == other.month && day > other.day);
-
-  @override
-  String toString()  => "$year, $month, $day";
+ Print operator +(Print other ) => Print(x + other.x, y + other.y);
+ Print operator -(Print other ) => Print(x - other.x, y - other.y);
+ @override
+ String toString() => "$x , $y";
 }
 
 void main(){
 
-  final d1 = Date(2025, 12, 19);
-  final d2 = Date(2024, 12, 31);
-  final d3 = Date(2025, 11, 20);
+  final p = Print(10, 12);
+  final p2 = Print(4, 20);
 
-  print(d1 > d2); // true
-  print(d1 > d3); // true
-  print(d3 > d1); // false
+  final sum = p + p2;
+  final sum2 = p - p2;
+  print("$sum, $sum2");
+
 }
